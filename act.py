788c65a4f4ab2e912f84688809434f2aaa2233a1
daniil <dan.naumihin@yandex.ru>
2020-03-02 16:11:54 +0300

    0.1

--- act.py
+++ act.py
@@ -7,6 +7,8 @@ from core.user_interface import Cursor
 from core.draw_something.object import ObjectImage
 import pygame
 
+
+# version 0.1
 pygame.init()
 deviceInfo = pygame.display.Info()
 pygame.display.set_icon(pygame.image.load(".data/icons/icon.png"))
